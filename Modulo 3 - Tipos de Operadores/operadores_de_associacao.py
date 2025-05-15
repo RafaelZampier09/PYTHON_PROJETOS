d450c59f815803eec7be6723136abe99c50420cc
RafaelZampier <rafaelzampier14@gmail.com>
2025-05-15 00:39:02 -0300

    Adicionando um novo modulo que se trata de tipos de operadores. Conta com um exemplo pratico que eu mesmo criei.

--- Modulo 3 - Tipos de Operadores/operadores_de_associacao.py
+++ Modulo 3 - Tipos de Operadores/operadores_de_associacao.py
@@ -1,8 +1,8 @@
 frutas = ["limao", "uva"]
 nome = "Rafael Zampier"
 
-print("laranja" in frutas) # Verifica se laranja esta dentro de frutas
-print("laranja" not in frutas) # Verifica se laranja NÃO esta dentro de frutas
+print("laranja" in frutas) # Verifica se laranja esta dentro de frutas.
+print("laranja" not in frutas) # Verifica se laranja NÃO esta dentro de frutas.
 
 print ("Rafael" in nome)
 print ("Rafael" not in nome)
